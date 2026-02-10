6113264c9fbbd02b3bae9bb5084f9cc1b208f931
HECTOR SANCHEZ <hector.alejandro11@hotmail.com>
2026-02-10 15:54:18 -0700

    l

--- models/product.js
+++ models/product.js
@@ -1,9 +1,17 @@
-const mongoose = require('mongoose');
+const Product = require('../models/Product');
 
-const ProductSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  userId: mongoose.Schema.Types.ObjectId
-});
-
-module.exports = mongoose.model('Product', ProductSchema);
+exports.create = async (req, res) => {
+  try {
+    const { name, price } = req.body;
+    // req.user.id viene del middleware de JWT que ya tienes
+    const newProduct = new Product({ 
+      name, 
+      price, 
+      userId: req.user.id 
+    });
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(500).json({ msg: "Error al crear producto" });
+  }
+};
